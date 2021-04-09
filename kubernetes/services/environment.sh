e032c3da406387e3943cda1eda0a35d1f6c9d652
Volad <vak.kondratenko@gmail.com>
2021-04-09 13:28:03 +0300

    moved to env variables

--- kubernetes/services/environment.sh
+++ kubernetes/services/environment.sh
@@ -4,6 +4,10 @@ export SEGMENTME_DB_CONNECTOR_URI="mongodb://192.168.1.14:27017/test?readPrefere
 export AUTH0_CLIENT_ID=FRFEhM5zwQCva2tSfBxDuM3og5TdjUpC
 export AUTH0_CLIENT_SECRET=Rsic0pW4Wr_q91Ppin-UNRuq5EdH5X7EmhA4ZUTcN-6MBu_Wy8Ce1J89WPCpkD_4
 export SME_BE_HOST=http://demo.segmentme.io:8756
-
+export ACCESS_SERVICE_HOST=http://localhost:8085
+export ANALYSIS_SERVICE_HOST=http://localhost:8082
+export MANAGEMENT_SERVICE_HOST=http://localhost:8084
+export CHANNEL_SERVICE_PORT=7777
+export CHANNEL_SERVICE_HOST=localhost
 echo "Environment variable set"
 
